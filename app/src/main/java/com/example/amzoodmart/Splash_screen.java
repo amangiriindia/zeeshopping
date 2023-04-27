1ab18af745056010ca9c05f50d24ae4210f462d1
Aman <amangiri381@gmail.com>
2023-04-28 00:07:10 +0530

    login System complete

--- app/src/main/java/com/example/amzoodmart/Splash_screen.java
+++ app/src/main/java/com/example/amzoodmart/Splash_screen.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
         SystemClock.sleep(3000);
         Intent i;
-        i = new Intent(MainActivity.this,RegistationActivity.class);
+        i = new Intent(Splash_screen.this,RegistationActivity.class);
         startActivity(i);
         finish();
-
     }
 }
