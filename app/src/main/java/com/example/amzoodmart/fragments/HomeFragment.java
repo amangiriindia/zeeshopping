a93da739e057ddab3e37ba34eea104e2829b4e68
Aman <amangiri381@gmail.com>
2023-04-30 20:07:22 +0530

    Firebase cloud connectivity complete and category work

--- app/src/main/java/com/example/amzoodmart/fragments/HomeFragment.java
+++ app/src/main/java/com/example/amzoodmart/fragments/HomeFragment.java
@@ -3,22 +3,36 @@ package com.example.amzoodmart.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.amzoodmart.R;
+import com.example.amzoodmart.adapters.CategoryAdapter;
+import com.example.amzoodmart.models.CategoryModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView catRecyclerView;
+    //Category recycleview
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
 
+    //FireStore
+    FirebaseFirestore db;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -29,6 +43,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.fragment_home2, container, false);
+        catRecyclerView =root.findViewById(R.id.rec_category);
+        db =FirebaseFirestore.getInstance();
+
 
         //image slider
         ImageSlider imageSlider =root.findViewById(R.id.image_slider);
@@ -39,6 +56,30 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3,"70% OFF", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+
+        // Category
+        catRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryModelList =new ArrayList<>();
+        categoryAdapter =new CategoryAdapter(getContext(),categoryModelList);
+        catRecyclerView.setAdapter(categoryAdapter);
+
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot querySnapshot = task.getResult();
+                        for (QueryDocumentSnapshot document : querySnapshot) {
+
+                            CategoryModel categoryModel =document.toObject(CategoryModel.class);
+                            categoryModelList.add(categoryModel);
+                            categoryAdapter.notifyDataSetChanged();
+                        }
+                    }
+//                    else {
+//
+//                    }
+                });
 
         return root;
     }
