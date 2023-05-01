21686d7bdb9e3a7baeb80a8b5a9433f531df1e9e
Aman <amangiri381@gmail.com>
2023-05-01 16:57:18 +0530

    New Product Work complete

--- app/src/main/java/com/example/amzoodmart/fragments/HomeFragment.java
+++ app/src/main/java/com/example/amzoodmart/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.amzoodmart.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -16,7 +17,9 @@ import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.amzoodmart.R;
 import com.example.amzoodmart.adapters.CategoryAdapter;
+import com.example.amzoodmart.adapters.NewProductAdapter;
 import com.example.amzoodmart.models.CategoryModel;
+import com.example.amzoodmart.models.NewProductsModel;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -26,10 +29,14 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView catRecyclerView;
+    RecyclerView catRecyclerView,newProductRecyclerView;
     //Category recycleview
     CategoryAdapter categoryAdapter;
     List<CategoryModel> categoryModelList;
+
+    //New product Recyclerview
+    NewProductAdapter newProductAdapter;
+    List<NewProductsModel> newProductsModelList;
 
     //FireStore
     FirebaseFirestore db;
@@ -38,12 +45,16 @@ public class HomeFragment extends Fragment {
     }
 
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.fragment_home2, container, false);
+
+
         catRecyclerView =root.findViewById(R.id.rec_category);
+        newProductRecyclerView =root.findViewById(R.id.new_product_rec);
         db =FirebaseFirestore.getInstance();
 
 
@@ -76,9 +87,35 @@ public class HomeFragment extends Fragment {
                             categoryAdapter.notifyDataSetChanged();
                         }
                     }
-//                    else {
-//
-//                    }
+                  else {
+                       // Toast.makeText(getActivity(), ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+                });
+
+        //new product
+        newProductRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        newProductsModelList =new ArrayList<>();
+        newProductAdapter =new NewProductAdapter(getContext(), newProductsModelList);
+        newProductRecyclerView.setAdapter(newProductAdapter);
+
+        db.collection("NewProduct")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot querySnapshot = task.getResult();
+                        for (QueryDocumentSnapshot document : querySnapshot) {
+
+                            NewProductsModel newProductsModel =document.toObject(NewProductsModel.class);
+                            newProductsModelList.add(newProductsModel);
+                            newProductAdapter.notifyDataSetChanged();
+                        }
+                    }
+                     else{
+                       // Toast.makeText(getActivity(), ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
                 });
 
         return root;
