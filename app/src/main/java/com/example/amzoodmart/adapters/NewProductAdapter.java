e10badf5fff485987b1324d48bd15164e78a2590
Aman <amangiri381@gmail.com>
2023-05-02 12:06:37 +0530

    Product Detailed Page Complete

--- app/src/main/java/com/example/amzoodmart/adapters/NewProductAdapter.java
+++ app/src/main/java/com/example/amzoodmart/adapters/NewProductAdapter.java
@@ -1,6 +1,8 @@
 package com.example.amzoodmart.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.amzoodmart.R;
+import com.example.amzoodmart.activities.DetailedActivity;
 import com.example.amzoodmart.models.NewProductsModel;
 
 import java.util.List;
@@ -33,10 +36,20 @@ public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Glide.with(context).load(list.get(position).getImg_url()).into(holder.newImg);
         holder.newName.setText(list.get(position).getName());
         holder.newPrice.setText(String.valueOf(list.get(position).getPrice()));
+
+        //for move to detail page
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(context, DetailedActivity.class);
+                intent.putExtra("detailed",list.get(position));
+                context.startActivity(intent);
+            }
+        });
 
     }
 
