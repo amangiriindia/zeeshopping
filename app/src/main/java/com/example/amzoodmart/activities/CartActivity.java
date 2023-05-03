b3d6b21112af993a8382ed9c7a17062387863bc6
Aman <amangiri381@gmail.com>
2023-05-04 00:55:27 +0530

    Database to displayed our data in mycart Activity

--- app/src/main/java/com/example/amzoodmart/activities/CartActivity.java
+++ app/src/main/java/com/example/amzoodmart/activities/CartActivity.java
@@ -1,16 +1,78 @@
 package com.example.amzoodmart.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.amzoodmart.R;
+import com.example.amzoodmart.adapters.MyCartAdapter;
+import com.example.amzoodmart.models.MyCartModel;
+import com.example.amzoodmart.models.ShowAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 
 public class CartActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    List<MyCartModel> cartModelsList;
+    MyCartAdapter cartAdapter;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        auth =FirebaseAuth.getInstance();
+        firestore =FirebaseFirestore.getInstance();
+        toolbar =findViewById(R.id.my_cart_toolbar_menu);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        recyclerView =findViewById(R.id.cart_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cartModelsList =new ArrayList<>();
+        cartAdapter =new MyCartAdapter(this,cartModelsList);
+        recyclerView.setAdapter(cartAdapter);
+
+        firestore.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("User").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (DocumentSnapshot doc :task.getResult().getDocuments()){
+
+                                MyCartModel myCartModel =doc.toObject(MyCartModel.class);
+                                cartModelsList.add(myCartModel);
+                                cartAdapter.notifyDataSetChanged();
+                            }
+                        }
+
+
+                    }
+                });
+
+
+
+
     }
 }
