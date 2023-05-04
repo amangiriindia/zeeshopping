6c1fa8a1c8aefccfe72800439bc8d589a01c4b6b
Aman <amangiri381@gmail.com>
2023-05-04 18:40:33 +0530

    Address display form the database and radio btn set

--- app/src/main/java/com/example/amzoodmart/activities/AddressActivity.java
+++ app/src/main/java/com/example/amzoodmart/activities/AddressActivity.java
@@ -1,23 +1,86 @@
 package com.example.amzoodmart.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.amzoodmart.R;
+import com.example.amzoodmart.adapters.AddressAdapter;
+import com.example.amzoodmart.models.AddressModel;
+import com.example.amzoodmart.models.MyCartModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class AddressActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class AddressActivity extends AppCompatActivity implements AddressAdapter.SelectedAddress {
 
     Button addAddress;
+    RecyclerView recyclerView;
+    private AddressAdapter addressAdapter;
+    private List<AddressModel> addressModelList;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+    Button addAddressBtn,paymentBtn;
+    Toolbar toolbar ;
+    String mAddress ="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
 
+        toolbar =findViewById(R.id.address_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        firestore =FirebaseFirestore.getInstance();
+        auth =FirebaseAuth.getInstance();
+
+        recyclerView = findViewById(R.id.address_recycler);
+        paymentBtn =findViewById(R.id.payment_btn);
         addAddress =findViewById(R.id.add_address_btn);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        addressModelList =new ArrayList<>();
+        addressAdapter =new AddressAdapter(getApplicationContext(),addressModelList,this);
+        recyclerView.setAdapter(addressAdapter);
+
+        firestore.collection("CurrentUser.").document(Objects.requireNonNull(auth.getCurrentUser()).getUid())
+                        .collection("Address").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (DocumentSnapshot doc :task.getResult().getDocuments()){
+
+                                AddressModel addressModel =doc.toObject(AddressModel.class);
+                                addressModelList.add(addressModel);
+                                addressAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+        paymentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddressActivity.this,PaymentActivity.class));
+            }
+        });
 
         addAddress.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +89,10 @@ public class AddressActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void setAddress(String address) {
+        mAddress =address;
     }
 }
--- app/src/main/java/com/example/amzoodmart/models/AddressModel.java
+++ app/src/main/java/com/example/amzoodmart/models/AddressModel.java
@@ -1,4 +1,27 @@
 package com.example.amzoodmart.models;
 
 public class AddressModel {
+    String userAddress;
+    boolean isSelected;
+
+    public AddressModel() {
+    }
+
+
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
 }
