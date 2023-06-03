f02b8232efe4e485e2328abb6f009c1a3e5d3f16
Aman <amangiri381@gmail.com>
2023-06-04 02:07:50 +0530

    some design change

--- app/src/main/java/com/example/amzoodmart/activities/MainActivity.java
+++ app/src/main/java/com/example/amzoodmart/activities/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer,R.string.closeDrawer);
-
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
