d9f0157afbd6c0fe0be198b6af3056f1a06d4536
Aman <amangiri381@gmail.com>
2024-05-03 00:23:53 +0530

    splash screen bug fixed

--- app/src/main/java/com/amzventures/zeeshopping/activities/Splash_screen.java
+++ app/src/main/java/com/amzventures/zeeshopping/activities/Splash_screen.java
@@ -1,30 +1,26 @@
 package com.amzventures.zeeshopping.activities;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.amzventures.zeeshopping.R;
 
 public class Splash_screen extends AppCompatActivity {
+
     private static final long SPLASH_DURATION = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Start your main activity
-                Intent intent = new Intent(Splash_screen.this, loginActivity.class);
-                startActivity(intent);
 
-                // Close the splash activity
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            // Start your main activity
+            Intent intent = new Intent(Splash_screen.this, loginActivity.class); // Assuming LoginActivity is the correct name
+            startActivity(intent);
+
+            // Close the splash activity
+            finish();
         }, SPLASH_DURATION);
-
     }
 }
